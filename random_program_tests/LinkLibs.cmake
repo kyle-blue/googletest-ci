328365108cb109c169a1f234ff7c703ad549c0f2
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2019-04-02 14:59:11 +0100

    Made dependencie local

--- random_program_tests/LinkLibs.cmake
+++ random_program_tests/LinkLibs.cmake
@@ -1,14 +1,14 @@
 function(linkGTestAndGMock EXE)
     if (TARGET ${EXE})
-        set(GTEST_DIR /usr/local/lib/gtest/)
-        set(GMOCK_DIR /usr/local/lib/gmock/)
+        set(GTEST_DIR lib/gtest/)
+        set(GMOCK_DIR lib/gmock/)
 
         #Must compile libary separately for each project. Read 'ReadMe.txt' in lib folder for details
         add_library(gtest ${GTEST_DIR}/src/gtest-all.cc)
         add_library(gmock ${GMOCK_DIR}/src/gmock-all.cc)
 
-        target_include_directories(gtest PUBLIC /usr/local/include/ ${GTEST_DIR})
-        target_include_directories(gmock PUBLIC /usr/local/include/ ${GMOCK_DIR})
+        target_include_directories(gtest PUBLIC include/ ${GTEST_DIR})
+        target_include_directories(gmock PUBLIC include/ ${GMOCK_DIR})
 
         target_link_libraries(${EXE} gtest gmock)
 
